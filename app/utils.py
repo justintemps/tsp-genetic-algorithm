a55e6d53262d4b0aa4103ca5baf36243bb06431e
justintemps <jesmith627@gmail.com>
2021-12-04 17:35:52 +0100

    Add get_city_distance util fx

---
+++ app/utils.py
@@ -0,0 +1,19 @@
+from math import sin, cos, radians
+from numpy import arccos
+
+
+def get_city_distance(city1, city2):
+    """
+    Take two pairs of long/lat coords <Tuple/List> and return the distance
+    between them in kilometers<Float>. First, convert degrees to radians so they
+    work with python trig functions.
+    """
+    EARTH_RADIUS = 6371.0  # in kilometers
+    city1_in_radians = (radians(city1.location[0]), radians(city1.location[1]))
+    city2_in_radians = (radians(city2.location[0]), radians(city2.location[1]))
+    return EARTH_RADIUS * arccos(sin(city1_in_radians[0]) *
+                                 sin(city2_in_radians[0]) +
+                                 cos(city1_in_radians[0]) *
+                                 cos(city2_in_radians[0]) *
+                                 cos(city1_in_radians[1] -
+                                     city2_in_radians[1]))
